089dfa5c718fa77224b14bdbe1c46af669ee303e
rob-has-a-headache <190219274+rob-has-a-headache@users.noreply.github.com>
2025-08-05 18:04:52 +0100

    basic logic added

--- script.js
+++ script.js
@@ -1,1 +1,35 @@
 console.log("This is a simple JavaScript file.");
+/* 
+choices =  3;
+
+function getComputerChoice(choices){
+    return Math.floor(Math.random()* choices);            
+}
+
+ */
+let humanChoice
+let ComputerChoice
+let humanScore = 0;
+let computerScore = 0;
+
+function getComputerChoice(){
+    ComputerChoice = Math.random();
+    //return ComputerChoice;
+    if(ComputerChoice>=0.5)
+    {ComputerChoice = "Rock";}
+    else {ComputerChoice="Scissors";}
+    return ComputerChoice;
+}
+
+function getHumanChoice(){
+    humanChoice = prompt("Select Rock, Paper or Scissors to play");
+}
+
+getHumanChoice()
+getComputerChoice()
+
+console.log("Computer choice: " + ComputerChoice)
+console.log("Human choice: " + humanChoice)
+console.log("Computer score: "+computerScore)
+console.log("Human Score: "+humanScore)
+console.log("end")
